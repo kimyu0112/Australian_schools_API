cd814c25f14c151464af22b2d4b81bebd98a7602
kimyu0112 <kimyu0112@outlook.com>
2024-07-16 23:42:49 +1000

    review model added

--- models/review.py
+++ models/review.py
@@ -4,23 +4,26 @@ from sqlalchemy.sql import func
 
 import datetime
 
-class Follow(db.Model):
-    __tablename__ = "follows"
+class Review(db.Model):
+    __tablename__ = "reviews"
 
     id = db.Column(db.Integer, primary_key=True)
+    review_title = db.Column(db.String, nullable=False)
+    review_content = db.Column(db.String, nullable=False)
     created_at_time = db.Column(db.DateTime(timezone=True), default=func.now())
 
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     school_id = db.Column(db.Integer, db.ForeignKey("schools.id"), nullable=False)
 
-    user = db.relationship('User', back_populates='follows')
-    school = db.relationship('School', back_populates='follows')
+    user = db.relationship('User', back_populates='reviews')
+    school = db.relationship('School', back_populates='reviews')
 
-class FollowSchema(ma.Schema):
+class ReviewSchema(ma.Schema):
     user = fields.Nested('UserSchema', only=["user_name"])
     school = fields.Nested('SchoolSchema', only=["school_name"])
-    class Meta:
-        fields = ("id", "user", "school", "created_at_time")
 
-follows_schema = FollowSchema(many=True)
-follow_schema = FollowSchema()
+    class Meta:
+        fields = ("id", "review_title", "review_content", "created_at_time", "user", "school")
+
+reviews_schema = ReviewSchema(many=True)
+review_schema = ReviewSchema()
--- models/user.py
+++ models/user.py
@@ -10,12 +10,14 @@ class User(db.Model):
     password = db.Column(db.String, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
-    follows = db.relationship('Follow', back_populates='user')
+    follows = db.relationship('Follow', back_populates='user', cascade="all, delete")
+    reviews = db.relationship('Review', back_populates='user', cascade="all, delete")
 
 class UserSchema(ma.Schema):
     follows = fields.List(fields.Nested('FollowSchema'), exclude=["user"])
+    reviews = fields.List(fields.Nested('ReviewSchema'), exclude=["user"])
     class Meta:
-        fields = ("id", "user_name", "email", "password", "is_admin", "follows")
+        fields = ("id", "user_name", "email", "password", "is_admin", "follows", "reviews")
 
 user_schema = UserSchema(exclude=["password"])
 users_schema = UserSchema(many=True, exclude=["password"])
