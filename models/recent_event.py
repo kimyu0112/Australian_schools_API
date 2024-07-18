48d1cece4f021c9f1eed981604107426278d35f8
kimyu0112 <kimyu0112@outlook.com>
2024-07-19 00:45:55 +1000

    models completed

---
+++ models/recent_event.py
@@ -0,0 +1,21 @@
+from init import db, ma
+from marshmallow import fields
+
+class Event(db.Model):
+    __tablename__ = "recent_events"
+
+    id = db.Column(db.Integer, primary_key=True)
+    event_title = db.Column(db.String, nullable=False)
+    event_brief_desciption = db.Column(db.String, nullable=False)
+
+    school_id = db.Column(db.Integer, db.ForeignKey("schools.id"), nullable=False)
+
+    school = db.relationship('School', back_populates='recent_events')
+
+class EventSchema(ma.Schema):
+    school = fields.Nested('SchoolSchema', only=["school_name"])
+    class Meta:
+        fields = ("id", "event_title", "event_content", "school")
+
+events_schema = EventSchema(many=True)
+event_schema = EventSchema()
